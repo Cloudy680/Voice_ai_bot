b24283b0d9bc8e547df8e442b87f06f2e6cd7031
Cloudy680 <yanushkevich2006@gmail.com>
2025-06-12 12:09:32 +0300

    Check

--- src/config.py
+++ src/config.py
@@ -1,9 +1,27 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+# from pydantic_settings import BaseSettings, SettingsConfigDict
+#
+# class Settings(BaseSettings):
+#     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+#
+#     TELEGRAM_BOT_TOKEN: str
+#     OPENAI_API_KEY: str
+#
+# settings = Settings()
+
+from pydantic_settings import BaseSettings
+from pydantic import Field, ValidationError
+
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+    OPENAI_API_KEY: str = Field(..., min_length=1)
+    TELEGRAM_BOT_TOKEN: str = Field(..., min_length=1)
 
-    TELEGRAM_BOT_TOKEN: str
-    OPENAI_API_KEY: str
 
-settings = Settings()
+    class Config:
+        env_file = ".env"
+
+
+try:
+    settings = Settings()
+except ValidationError as e:
+    raise Exception(f"Ошибка загрузки настроек: {e}") from e
