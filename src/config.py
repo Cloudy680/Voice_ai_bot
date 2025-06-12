430a69d5214625d087f4f22e67246864d80fc5e6
Cloudy680 <yanushkevich2006@gmail.com>
2025-06-12 11:18:52 +0300

    Railway deploy

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+    #model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
     telegram_bot_token: str
     openai_api_key: str
